3da76e531aa908eb306edc2e57e9b7cf2b2ab9ea
Roland Meneghetti <roland.meneghetti@gmail.com>
2023-03-04 22:38:56 +0000

    add build and deploy stuff

---
+++ build.clj
@@ -0,0 +1,23 @@
+(ns build
+  (:require [clojure.string :as str]
+            [clojure.tools.build.api :as b]))
+
+(def lib 'net.roghetti/itt)
+(def version (-> "VERSION" slurp str/trimr))
+(def class-dir "target/classes")
+(def basis (b/create-basis {:project "deps.edn"}))
+(def jar-file (format "target/%s.jar" (name lib)))
+
+(defn clean [_]
+  (b/delete {:path "target"}))
+
+(defn jar [_]
+  (b/write-pom {:class-dir class-dir
+                :lib lib
+                :version version
+                :basis basis
+                :src-dirs ["src"]})
+  (b/copy-dir {:src-dirs ["src"]
+               :target-dir class-dir})
+  (b/jar {:class-dir class-dir
+          :jar-file jar-file}))
---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+clj -T:build clean
+clj -T:build jar
+rm -rf ziptmp
+unzip target/itt.jar -d ziptmp
+tail -n+3 ziptmp/META-INF/maven/net.roghetti/itt/pom.properties > ziptmp/META-INF/maven/net.roghetti/itt/pom.properties.new
+mv ziptmp/META-INF/maven/net.roghetti/itt/pom.properties.new ziptmp/META-INF/maven/net.roghetti/itt/pom.properties
+find ziptmp -exec touch -t 198001010000 {} +
+cd ziptmp
+zip -r itt.jar *
+cd ..
+mv ziptmp/itt.jar target/itt.jar
+cp ziptmp/META-INF/maven/net.roghetti/itt/pom.xml pom.xml
+touch -t 198001010000 pom.xml
+rm -rf ziptmp
+strip-nondeterminism -t jar target/itt.jar
